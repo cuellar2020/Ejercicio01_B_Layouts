fb3693d1588cec6fe0741c8ae6f6e2f1e0b5dfcb
cuellar2020 <67763251+cuellar2020@users.noreply.github.com>
2022-04-09 18:48:41 -0400

    Add project files.

---
+++ Ejercicio01_B/Ejercicio01_B/StackLoyoutCode.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace Ejercicio01_B
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackLoyoutCode : ContentPage{
+        public StackLoyoutCode(){
+            InitializeComponent();
+            var layout = new StackLayout();
+            var button = new Button
+            {
+                Text = "StackLayout",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            var yellowBox = new BoxView { Color = Color.Yellow,
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            var greenBox = new BoxView
+            {
+                Color = Color.Green,
+                VerticalOptions= LayoutOptions.FillAndExpand,
+                HorizontalOptions= LayoutOptions.FillAndExpand,
+            };
+            var blueBox = new BoxView
+            {
+                Color = Color.Blue,
+                VerticalOptions=LayoutOptions.FillAndExpand,
+                HorizontalOptions=LayoutOptions.FillAndExpand,
+            };
+            layout.Children.Add(button); 
+            layout.Children.Add(yellowBox);
+            layout.Children.Add(greenBox);
+            layout.Children.Add(blueBox);
+            layout.Spacing = 10;
+            Content = layout;
+        }
+    }
+}
